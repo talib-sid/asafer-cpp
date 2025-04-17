757a72b773a672c3b89f32e3317e2b812305c51e
username <talib.siddiqui@students.iiit.ac.in>
2025-04-18 04:43:08 +0530

    removed AST stuff for new branch

---
+++ src/AST/MyASTConsumer.hpp
@@ -0,0 +1,25 @@
+#pragma once
+// #include <clang/AST/ASTConsumer.h>
+
+#include "MyASTVisitor.hpp"
+
+
+class MyASTConsumer : public clang::ASTConsumer {
+
+    virtual void anchor();
+    public:
+    MyASTConsumer(clang::ASTContext *Context, AllocationTable &table)
+        : Visitor(Context, table) {}
+
+    void HandleTranslationUnit(clang::ASTContext &Context) override {
+        Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+    }
+
+private:
+    MyASTVisitor Visitor;
+
+inline void MyASTConsumer::anchor() {}
+
+
+
+
