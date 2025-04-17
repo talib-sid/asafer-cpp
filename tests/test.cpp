a1564f0556927a4a4a48f1500b25a4c6ded3ff58
username <talib.siddiqui@students.iiit.ac.in>
2025-04-17 19:03:21 +0530

    prog and test files updated

---
+++ tests/test.cpp
@@ -0,0 +1,49 @@
+// // some custom Class
+// class A
+// {
+// public:
+//     int a;
+//     float b;
+// };
+
+
+// template<typename T>
+// class B
+// {
+// public:
+//     T c;
+//     double d;
+// };
+
+// template<typename T, typename U>
+// class C
+// {
+// public:
+//     T e;
+//     U f;
+// };
+
+
+// int main() {
+//     int x = 5;
+    
+//     A objA;
+
+//     objA.a = 10;
+//     objA.b = 20.5f;
+
+//     B <int> objB;
+//     objB.c = 30;
+//     objB.d = 40.5;
+
+//     C <int, float> objC;
+//     objC.e = 50;
+//     objC.f = 60.5f;
+
+//  // float y = 3.14f;
+// }
+
+int main(){
+    int a = 6;
+}
+
