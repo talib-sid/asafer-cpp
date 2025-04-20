06991b72de5a9781a41e207641d631e73f7cac33
username <talib.siddiqui@students.iiit.ac.in>
2025-04-20 20:22:04 +0530

    Added DeleteInfo and DeleteAttempt to track with AllocTable for double-deletes (working)

--- src/MemoryTracker/AllocationTable.hpp
+++ src/MemoryTracker/AllocationTable.hpp
@@ -1,7 +1,11 @@
 #pragma once
 #include <map>
+#include <vector>
 #include <string>
+
 #include <clang/Basic/SourceLocation.h>
+#include <clang/Basic/SourceManager.h>
+#include <llvm/Support/raw_ostream.h>
 
 class AllocationTable {
 public:
@@ -9,21 +13,41 @@ public:
         allocations[var] = {true, loc};
     }
 
-    bool isAllocated(const std::string &var) const {
+    bool wasEverAllocated(const std::string &var) const {
+        return allocations.find(var) != allocations.end();
+    }
+    
+    bool isCurrentlyAllocated(const std::string &var) const {
         auto it = allocations.find(var);
         return it != allocations.end() && it->second.allocated;
     }
+    
 
     void markFreed(const std::string &var) {
         if (allocations.count(var))
             allocations[var].allocated = false;
     }
+
     
-    bool isFreed(const std::string &var) const {
-        auto it = allocations.find(var);
-        return it != allocations.end() && !it->second.allocated;
+    void recordDeleteAttempt(const std::string &var, clang::SourceLocation loc) {
+        deletes.push_back({var, loc});
     }
-    
+
+
+    void finalizeDeletes() {
+        for (const auto &d : deletes) {
+            llvm::outs() << "[DELETE] Attempt to delete: " << d.varName << "\n";
+
+            if (!wasEverAllocated(d.varName)) {
+                llvm::outs() << " Warning: '" << d.varName << "' was not allocated with new!\n";
+            } else if (!isCurrentlyAllocated(d.varName)) {
+                llvm::outs() << "Error: Double delete detected for '" << d.varName << "'\n";
+            } else {
+                markFreed(d.varName);
+                llvm::outs() << "Delete OK: " << d.varName << "\n";
+            }
+        }
+    }
 
 private:
     struct AllocationInfo {
@@ -31,5 +55,11 @@ private:
         clang::SourceLocation location;
     };
 
+    struct DeleteInfo {
+        std::string varName;
+        clang::SourceLocation location;
+    };
+
     std::map<std::string, AllocationInfo> allocations;
+    std::vector<DeleteInfo> deletes;
 };
